233ae35b33bee21de2750adaacd07cfa5a40d1d8
Crypto-kittensSeller <Nikolay.Shaveiko@gmail.com>
2019-06-24 12:25:20 +0300

    refactor: improve performance of db updating script

--- config/config.js
+++ config/config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: process.env.NODE_MONGODB_URI
+  mongoURI:
+    process.env.NODE_MONGODB_URI ||
+    'mongodb+srv://jedi:star_wars@cluster0-synnb.mongodb.net/development?retryWrites=true&w=majority'
 };
--- database/updateDBfromSwapi.js
+++ database/updateDBfromSwapi.js
@@ -8,23 +8,24 @@ async function updateDB() {
     while (nextData) {
       const res = await axios.get(url);
 
-      for (char of res.data.results) {
-        try {
-          if (char.homeworld) {
-            const planet = await axios.get(char.homeworld);
-            char.homeworld = planet.data.name;
-          }
-          await charController.addCharacter(char);
-        } catch (err) {
-          console.error(err.message);
+      const chunkOfChars = res.data.results.map(async char => {
+        if (char.homeworld) {
+          const planet = await axios.get(char.homeworld);
+          char.homeworld = planet.data.name;
         }
-      }
-      console.log('Chunk of characters received and added to database');
+        await charController.addCharacter(char);
+      });
+
+      Promise.all(chunkOfChars)
+        .then(() => {
+          console.log('Chunk of characters received and added to database');
+        })
+        .catch(err => console.log(err));
+
       if (res.data.next) {
         url = res.data.next;
       } else {
         nextData = false;
-        console.log('All characters have been received and added to database');
       }
     }
   } catch (err) {
